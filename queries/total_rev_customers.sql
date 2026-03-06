b3e96ab990043e9268246c3727daf1832f77c52e
Gio-A-516 <gioarcari3@gmail.com>
2026-03-06 15:05:44 -0500

    Create query for total revenue by customer
    Add SQL query to calculate total revenue per customer

--- queries/total_rev_customers.sql
+++ queries/total_rev_customers.sql
@@ -1,1 +1,9 @@
+Get total revenue per customer
 
+SELECT c.name, sum(p.amount) AS total_revenue
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id
+JOIN payments p
+	ON o.order_id = p.order_id
+GROUP BY c.name;
