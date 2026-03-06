3bb4f742295fe87c66cf726634da6d1e0bf23867
Gio-A-516 <gioarcari3@gmail.com>
2026-03-06 15:03:10 -0500

    Fix SQL syntax for credit card payment query

--- queries/CC_order_details.sql
+++ queries/CC_order_details.sql
@@ -1,7 +1,7 @@
 Show the order_id and payment amount for orders paid using 'Credit Card'
 
-select o.order_id, p.amount
-from orders o 
-inner join on payments p
-    on o.order_id = p.order_id 
-where p.payment_method = 'Credit Card';
+SELECT o.order_id, p.amount
+FROM orders o 
+INNER JOIN payments p
+    ON o.order_id = p.order_id 
+WHERE p.payment_method = 'Credit Card';
