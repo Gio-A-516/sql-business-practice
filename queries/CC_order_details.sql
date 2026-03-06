e8512da0e675d70fe1135085237669594902075b
Gio-A-516 <gioarcari3@gmail.com>
2026-03-06 14:59:00 -0500

    Update SQL query for Credit Card order details
    Fix SQL query to correctly join orders and payments.

--- queries/CC_order_details.sql
+++ queries/CC_order_details.sql
@@ -1,1 +1,7 @@
+Show the order_id and payment amount for orders paid using 'Credit Card'
 
+select o.order_id, p.amount
+from orders o 
+inner join on payments p
+    on o.order_id = p.order_id 
+where p.payment_method = 'Credit Card';
