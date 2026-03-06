0267e512f2c748b48d00da8371f35696c19c3c93
Gio-A-516 <gioarcari3@gmail.com>
2026-03-06 15:07:15 -0500

    Update customers_several_orders.sql
    Query retrieves customers with more than two orders.

--- queries/customers_several_orders.sql
+++ queries/customers_several_orders.sql
@@ -1,1 +1,8 @@
+Select customers with at least 2 orders
 
+select c.customer_id, count(o.order_id) as order_count
+from customers c
+join orders o
+	on c.customer_id = o.customer_id
+group by c.customer_id
+having count(o.order_id) > 2;
